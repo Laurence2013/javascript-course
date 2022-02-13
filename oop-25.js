931066414c230adebd6e199f7fa9972585091f22
Laurence2013 <lozza244@protonmail.com>
2022-02-13 02:38:32 +0000

    #11-commit

--- oop-25.js
+++ oop-25.js
@@ -1,12 +1,37 @@
 let minion = {
 	hp: 10,
-	fName: 'Laurence',
-	lName: 'Mitchell',
+	name: 'minion',
 	toString(){
-		return this.fName;
+		return this.name;
 	}
 };
 let giantScorpion = {
 	'__proto__': minion,
-	
+	name: 'giant scorpion',
+	stings(){
+		console.log(`${this} pierces your shoulders with venemous stings`);
+	}
 }
+let smallScorpion = {
+	'__proto__': minion,
+	name: 'small scorpion',
+	stings(){
+		console.log(`${this} pierces your shoulders with venemous stings`);
+	}
+}
+let giantSpider = {
+	'__proto__': minion,
+	name: 'giant spider',
+	stings(){
+		console.log(`${this} launches a sticky web and immobilises you`);
+	}
+}
+
+minion.eats = function(food){
+	console.log(`${this} eats ${food} and gains ${food.hp} health`);
+	this.hp += food.hp;
+}
+
+giantScorpion.eats({name: 'hamburger', hp: 10, toString(){return this.name}});
+smallScorpion.eats({name: 'ice cream', hp: 11, toString(){return this.name}});
+giantSpider.eats({name: 'hamburger', hp: 100, toString(){return this.name}});
