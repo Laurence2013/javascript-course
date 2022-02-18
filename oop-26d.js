f2328de7e25286d8ab56658aff256b42e9f96402
Laurence2013 <lozza244@protonmail.com>
2022-02-18 22:17:32 +0000

    #15-commit

---
+++ oop-26d.js
@@ -0,0 +1,32 @@
+function Person(name, address){
+	this.name = name;
+	this.address = address;	
+}
+Person.prototype.toString = function(){
+	return `name: ${this.name} address: ${this.address}`;
+}
+
+const myPerson = new Person('John Doe', '123 Some Address');
+
+console.log(Object.keys(myPerson));
+console.log('\n');
+console.log(myPerson.hasOwnProperty('toString'));
+console.log(Person.hasOwnProperty('toString'));
+console.log(Person.prototype.hasOwnProperty('toString'));
+console.log('\n');
+console.log(myPerson.toString());
+console.log('\n');
+console.log(Person.prototype.toString.call(myPerson));
+console.log('\n');
+console.log(Person.prototype.toString.call({
+	name: 'Laurence Mitchell',
+	address: '456 Some Address'
+}));
+console.log('\n');
+
+const myPerson00 = Person.prototype.toString.call({
+	name: 'Craig Write',
+	address: '789 Some Address'
+});
+
+console.log(myPerson00);
